e19861eca99d37478621ca00f4adb3977c36def1
Wilson Gramer <wilson@gramer.dev>
2021-03-15 13:20:33 -0400

    Update build script

--- build.rs
+++ build.rs
@@ -5,9 +5,22 @@ fn main() {
     println!("cargo:rustc-link-lib=botball");
     println!("cargo:rerun-if-changed=include/wrapper.h");
 
+    let ignored_macros = IgnoreMacros(
+        vec![
+            String::from("FP_INFINITE"),
+            String::from("FP_NAN"),
+            String::from("FP_NORMAL"),
+            String::from("FP_SUBNORMAL"),
+            String::from("FP_ZERO"),
+        ]
+        .into_iter()
+        .collect(),
+    );
+
     let bindings = bindgen::Builder::default()
         .header("include/wrapper.h")
-        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .parse_callbacks(Box::new(ignored_macros))
+        .rustfmt_bindings(true)
         .generate()
         .expect("Unable to generate bindings");
 
@@ -15,4 +28,17 @@ fn main() {
     bindings
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Couldn't write bindings!");
+}
+
+#[derive(Debug)]
+struct IgnoreMacros(std::collections::HashSet<String>);
+
+impl bindgen::callbacks::ParseCallbacks for IgnoreMacros {
+    fn will_parse_macro(&self, name: &str) -> bindgen::callbacks::MacroParsingBehavior {
+        if self.0.contains(name) {
+            bindgen::callbacks::MacroParsingBehavior::Ignore
+        } else {
+            bindgen::callbacks::MacroParsingBehavior::Default
+        }
+    }
 }
